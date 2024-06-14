135cae85131cb435bf1699ae042ed1e6cea7a229
Moktadir.H <moktadirh22@gmail.com>
2024-06-13 22:26:35 -0400

    r peak detection

--- Functions.py
+++ Functions.py
@@ -1,3 +1,38 @@
-def somethinga(a,b):
-    c=a+b
-    return c
+
+
+def detect_peaks(ecgSignal):
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from scipy.signal import find_peaks
+
+
+    # Compute the moving average of the ECG signal
+    #larger window size more smoothing but less visible sharp features, smaller are more sensitive to noise
+    #At each point, find average of data in the window to make a "smoother version, the window moves and groups an area into one point to smoothen it out
+    #Window size just depends on just the ecg signal
+    windowSize = int(0.12 * len(ecgSignal))
+
+    #mode same means that the output has the same length as the input signal, so the actual graph doesnt become shorter or bigger
+    #Convolve is how the moving average is computer
+    movingAvg = np.convolve(ecgSignal, np.ones(windowSize) / windowSize, mode="same")
+
+    # Calc std dev of moving average
+    stdDev = np.std(movingAvg)
+
+    # Set high and low thresholds, and is dynamic so it can be used in many different files
+    #A valid r peak comes when it satisfies both thresholds, so that it can get precise with the low value but only goes throigh if the high also allows, letting it refine it further
+    threshold_high = 0.6 * stdDev
+    threshold_low = 0.4 * stdDev
+
+    # Find peaks in the moving average
+    #Distance is half the window size so that it doesnt detect the QRS complex multiple times
+    peaks, _ = find_peaks(movingAvg, height=threshold_high, distance=(windowSize//2))
+
+    # Refine peaks using the lower threshold
+    refinedPeaks, _ = find_peaks(movingAvg, height=threshold_low, distance=(windowSize//2))
+
+    # Keep only the peaks that satisfy both thresholds
+    finalPeaks = np.intersect1d(peaks, refinedPeaks)
+
+    return finalPeaks
--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import Functions as of
 import matplotlib.pyplot as plt
 from scipy import signal
 import neurokit2 as neuro
 import numpy as np
 import pywt
+import Functions as fx
 
 columns = ["lead1","lead2", "lead3", "Time"]
 time=0
@@ -24,7 +24,7 @@ times = range(0, rowCount * timeCheck, timeCheck)
 ecg["Time"] = list(times)
 
 order = 4  #4th order Filter (Butterworth)
-b, a = signal.butter(order,0.1, btype="low", analog=False)
+b, a = signal.butter(order,0.09, btype="low", analog=False)
 
 #Remove noise and other things just to make it easier
 filteredEcg = pd.DataFrame()
@@ -36,35 +36,34 @@ filteredEcg["lead2"] = signal.filtfilt(b, a, ecg.lead2)
 filteredEcg["lead3"] = signal.filtfilt(b, a, ecg.lead3)
 
 
-#Following Pan-Tompkins algorithm
+"""#Following Pan-Tompkins algorithm
 #Change Height accordingly since we do not know if the bottom ones are needed
 peaks1,_=signal.find_peaks(filteredEcg.lead1, height=0.2)
-peaks2,_=signal.find_peaks(filteredEcg.lead2, height=0.2)
-peaks3,_=signal.find_peaks(filteredEcg.lead3, height=0.2)
 
 
 #Following Pan-Tompkins algorithm
 #Differentiating and squaring the filtered ecg as lead 1 only, helps really find those peak slopes which are the r peaks, make it more visible
-diffEcg=np.diff(filteredEcg.lead1)
-squaredEcg = diffEcg ** 2
+#One index smaller so whatever the peaks are shpuld be changed by 5ms since that is one index
+diffEcg=np.gradient((filteredEcg.lead1*20),1)
+squaredEcg=diffEcg ** 2
 
 #larger window size more smoothing but less visible sharp features, smaller are more sensitive to noise
 #At each point, find average of data in the window to make a "smoother version, the window moves and groups an area into one point to smoothen it out
 #0.12 seconds is usually the duration of the QRS complex
-windowSize = int(0.12 * 200)  # Adjust to whatever works
+windowSize = int(0.1 * 200)  # Adjust to whatever works
 
 #mode same means that the output has the same length as the input signal, so the actual graph doesnt become shorter or bigger
 #Convolve is how the moving average is computer
 movingAverage = np.convolve(squaredEcg, np.ones(windowSize) / windowSize, mode="same")
+findPoints,_=signal.find_peaks(movingAverage, height=0.02, distance=35)
+findPoints= [x for x in findPoints if x >=0.2]"""
 
-# Thresholding to distinguish the r peaks, value depends on other factors and higher may miss actual r peaks but get rid of noise
-threshold = 0.0005
-#index 0 since where function gives a tuple of arrays and we just need the first one since it is a 1d array
-rPeaks = np.where(movingAverage> threshold)[0]
-
+rPeaks=fx.detect_peaks(filteredEcg.lead1)
 
 plt.figure()
 plt.plot(filteredEcg.Time, filteredEcg.lead1, label="Lead 1")
+
+#Since derivative changes the size, the -4 calibrates it back from the sine graph to the actual graph
 plt.scatter(filteredEcg.Time[rPeaks], filteredEcg.lead1[rPeaks], color="r", marker="o", label="R-peaks")
 plt.xlabel("Time (ms)")
 plt.ylabel("Amplitude (mV)")
